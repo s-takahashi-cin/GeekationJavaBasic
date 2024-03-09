22b3af8afa205715ed339904d5ca088faa5da2a2
s-takahashi-cin <s.takahashi.cingroup2012@gmail.com>
2024-03-08 21:23:26 -0800

    commi 変数実装

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,14 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num1 = 2;
+		double num2 = 2.50;
+		
+		System.out.println("私の好きな整数は"+num1+"です。");
+		System.out.println("私の好きな少数は"+num2+"です。");
+	}
+
+}
