a88ee5eae65d91acdbc182ccc1d7d8ee839609aa
Sawa Takahashi <161792420+s-takahashi-cin@users.noreply.github.com>
2024-03-12 00:31:07 -0700

    modified spacing

--- src/basicSyntax/For_While.java
+++ src/basicSyntax/For_While.java
@@ -5,11 +5,11 @@ public class For_While {
 	public static void main(String[] args) {
 		// 問題１
 		for(int i = 1; i <= 100; i++) {
-				if(i % 3== 0 && i%5==0) {
+				if(i % 3 == 0 && i % 5 == 0) {
 					System.out.println("FizzBuzz");
-				}else if(i %3==0) {
+				}else if(i % 3 == 0) {
 					System.out.println("Fizz");
-				}else if(i%5==0){
+				}else if(i % 5 == 0){
 					System.out.println("Buzz");
 				}else {
 					System.out.println(i);
