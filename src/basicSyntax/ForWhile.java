bd2f3a776c9b63b9d462df6a3f95f736fb254105
s-takahashi-cin <s.takahashi.cingroup2012@gmail.com>
2024-03-12 21:48:58 -0700

    commit ForWhile.java

--- src/basicSyntax/ForWhile.java
+++ src/basicSyntax/ForWhile.java
@@ -4,12 +4,12 @@ public class For_While {
 
 	public static void main(String[] args) {
 		// 問題１
-		for(int i = 1; i <= 100; i++) {
+		for (int i = 1; i <= 100; i++) {
 				if(i % 3 == 0 && i % 5 == 0) {
 					System.out.println("FizzBuzz");
-				} else if(i % 3 == 0) {
+				} else if (i % 3 == 0) {
 					System.out.println("Fizz");
-				} else if(i % 5 == 0){
+				} else if (i % 5 == 0){
 					System.out.println("Buzz");
 				} else {
 					System.out.println(i);
@@ -20,8 +20,8 @@ public class For_While {
 		
 		
 		//問題２
-		for(String language: languages) {
-			if(language == "Java") {
+		for (String language: languages) {
+			if (language == "Java") {
 				System.out.println("現在学習中の言語はJavaです。");
 				continue;
 			}else if(language == "HTML") {
