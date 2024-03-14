83596f4471302b93b7694e9814aac2a6d98e3ecc
Sawa Takahashi <161792420+s-takahashi-cin@users.noreply.github.com>
2024-03-13 19:07:25 -0700

    Update ForWhile.java

--- src/basicSyntax/ForWhile.java
+++ src/basicSyntax/ForWhile.java
@@ -5,11 +5,7 @@ public class ForWhile {
 	public static void main(String[] args) {
 		// 問題１
 		for (int i = 1; i <= 100; i++) {
-<<<<<<< HEAD
-				if(i % 3 == 0 && i % 5 == 0) {
-=======
 				if　(i % 3 == 0 && i % 5 == 0) {
->>>>>>> main
 					System.out.println("FizzBuzz");
 				} else if (i % 3 == 0) {
 					System.out.println("Fizz");
