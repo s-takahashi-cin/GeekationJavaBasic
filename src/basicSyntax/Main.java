2520ee50572956ba371d71e337117099df56757a
s-takahashi-cin <s.takahashi.cingroup2012@gmail.com>
2024-03-13 18:48:43 -0700

    commit GeekIntroduction/Main file

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,16 @@
+package basicSyntax;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int age = 30;
+		double height = 175.0;
+		int num1 = 15;
+		int num2 = 30;
+		
+		GeekIntroduction.greeding(age, height);
+		GeekIntroduction.specialSkill(num1, num2);
+		
+	}
+
+}
